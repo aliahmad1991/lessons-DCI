0150a72a43d9df457042f1fbfcbba70c6178b917
aliahmad1991 <aliahmadheydarzadeh@gmail.com>
2019-08-27 11:14:04 +0200

    CONVERTER

--- Javascript/js/Drag and Drop/example/example.js
+++ Javascript/js/Drag and Drop/example/example.js
@@ -11,8 +11,16 @@ function drop(e){
     let data = e.dataTransfer.getData("content");
     let img = document.getElementById(data);
     e.target.appendChild(img);
+    e.target.className="dragdrop";
 }
 function dragOver(e){
     e.preventDefault();
 }
-
+function dragEnter(e){
+    e.preventDefault();
+    e.target.className+=" hovering";
+}
+function dragLeave(e){
+    e.target.classList.remove("hovering");
+    
+}
