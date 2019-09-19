18260a95cb1b64e529a81f15e757abffe7af56ed
aliahmad1991 <aliahmadheydarzadeh@gmail.com>
2019-09-19 15:53:21 +0200

    registration Form

--- Javascript/js/exercise/register Form/main.js
+++ Javascript/js/exercise/register Form/main.js
@@ -11,14 +11,14 @@ function nextDiv(e){
     let checkLastName = document.getElementsByName("lastName")[0].checkValidity();
     let checkBirthDate = document.getElementsByName("birthDate")[0].checkValidity();
     let checkNationality = document.getElementsByName("nationality")[0].checkValidity();
-
+    let checkuName = document.getElementsByName("userName")[0].checkValidity();
     let checkAddress=document.getElementsByName("address")[0].checkValidity();
     let checkHousNumber=document.getElementsByName("houseNumber")[0].checkValidity();
     let checkZipCode=document.getElementsByName("zipCode")[0].checkValidity();
     let checkpassword=document.getElementsByName("password")[0].checkValidity();
     let passwordMatch=
     document.getElementsByName("password")[0].value === document.getElementsByName("repeatPassword")[0].value;
-
+let checkImageFile=document.getElementById("imageFile").checkValidity();
    // alert('Next btn is clicked');
 let parentDivId = e.target.parentElement.id;
 //alert(parentDivId);
@@ -39,15 +39,18 @@ switch(parentDivId){
         document.getElementById("profileInfo").style.display="block";
         break;
     case 'profileInfo' :
+        if(checkuName && checkImageFile && passwordMatch){
         e.target.parentElement.style.display="none";
         document.getElementById("addressDiv").style.display="none";
         document.getElementById("personalDiv").style.display="none";
         document.getElementById("resultDiv").style.display="block";
-
-
+        showDate();
+        }
+        else{
+            alert("You have invalid entries ");
+        }
+        break;
 }
-
-
 }
 function previousDiv(e){
     e.preventDefault();
@@ -84,3 +87,10 @@ btns.forEach(function(item){
         item.addEventListener("click",previousDiv);
     }
 })
+
+function showDate(){
+    let fName = document.getElementsByName("firstName")[0].value;
+    let lName=document.getElementsByName("lastname")[0].value;
+    let bDate=document.getElementsByName("birthDate")[0].value;
+    let nationality=document.getElementsByName("nationality")[0].value;
+}
--- Javascript/js/fetch/fetch-Examples/main-fetch.js
+++ Javascript/js/fetch/fetch-Examples/main-fetch.js
@@ -1,8 +1,13 @@
 let btn1=document.getElementById("btn1");
 let btn2=document.getElementById("btn2");
+let btn3=document.getElementById("btn3");
+
+
 
 btn1.addEventListener("click",getText);
 btn2.addEventListener("click",getJson);
+btn3.addEventListener("click",getExternal);
+
 
 //Get Local Text file data
 function getText(){
@@ -20,3 +25,45 @@ return res.text();
     console.log(err);
 })
 }
+//Get Local Json file data
+function getJson(){
+    fetch('posts.json')
+    .then(function(res){
+    return res.json();
+    
+    })
+    .then(function(info){
+        console.log(info);
+       let output="";
+       info.forEach(function(post){
+            output+= `<li> ${post.title} </li>`;
+            
+        });
+        document.getElementById("output").innerHTML=output;
+      
+    })
+    .catch(function(err){
+        console.log(err);
+    })
+    }
+   // get from External Api
+    function getExternal(){
+        fetch('https://api.github.com/users')
+        .then(function(res){
+        return res.json();
+        
+        })
+        .then(function(info){
+            console.log(info);
+           let output="";
+           info.forEach(function(user){
+                output+= `<li> ${user.avatar_url} </li>`;
+                
+            });
+            document.getElementById("output").innerHTML=output;
+          
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+        }
