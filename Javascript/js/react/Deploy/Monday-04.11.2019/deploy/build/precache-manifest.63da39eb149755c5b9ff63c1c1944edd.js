e1ecb1aff5558f3f1c9583dc6f17b35473e455a5
aliahmad1991 <aliahmadheydarzadeh@gmail.com>
2019-11-04 14:20:50 +0100

    commit

---
+++ Javascript/js/react/Deploy/Monday-04.11.2019/deploy/build/precache-manifest.63da39eb149755c5b9ff63c1c1944edd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a581510a3db243ed718828783dc97c1c",
+    "url": "/Monday-04.11.2019/index.html"
+  },
+  {
+    "revision": "b1897205a006838693ca",
+    "url": "/Monday-04.11.2019/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "9c5e349caa8c0dcb13e6",
+    "url": "/Monday-04.11.2019/static/js/2.ea0fe5d6.chunk.js"
+  },
+  {
+    "revision": "b1897205a006838693ca",
+    "url": "/Monday-04.11.2019/static/js/main.0117b2f4.chunk.js"
+  },
+  {
+    "revision": "632bfd01454c8bfeaea6",
+    "url": "/Monday-04.11.2019/static/js/runtime-main.bd634505.js"
+  }
+]);
