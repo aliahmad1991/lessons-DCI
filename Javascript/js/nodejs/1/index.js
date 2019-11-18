0efd6fbc957553008e17f0210a73ce7dc49fc12a
aliahmad1991 <aliahmadheydarzadeh@gmail.com>
2019-11-18 10:11:47 +0100

    456

---
+++ Javascript/js/nodejs/1/index.js
@@ -0,0 +1,8 @@
+function hello(fullname){
+    console.log(`Hi ${fullname} the time is 
+    ${new Date().getHours()} Hours
+    ${new Date().getMinutes()} Minutes !
+    `);
+
+}
+hello('Ghandolaks')
