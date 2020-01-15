6474d53aa0d80383df5fe21d5f9621ec361ba034
aliahmad1991 <aliahmadheydarzadeh@gmail.com>
2020-01-15 15:12:00 +0100

    new

---
+++ Dataserver/devcamp/controllers/bootcamps.js
@@ -0,0 +1,36 @@
+
+const Bootcamp = require('../models/Bootcamp');
+
+// get all the bootcamps without id 
+exports.getBootcamps = (req ,res , next) => {
+    res.status(200).json({ success : true , msg : ' Show all bootcamps' , hello:req.hello , host:req.info});
+}
+
+// get a single bootcamp using id 
+exports.getBootcamp = (req ,res , next) => {
+    res.status(200).json({ success : true , msg : ` Show  bootcamps ${req.params.id}`});
+}
+
+
+exports.createBootcamp = async (req , res ,next) => {
+  try{ // console.log(req.body);
+    const bootcamp = await Bootcamp.create(req.body);
+
+    res.status(201).json({success:true , data:bootcamp});
+  }catch{
+    
+    res.status(400).json({ success : true ,  msg : ` Create new bootcamps`});
+  }
+
+}
+
+exports.updateBootcamp = (req,res , next) => {
+    res.status(200).json({ success : true , msg : ` Update  bootcamps ${req.params.id}`});
+
+
+}
+
+exports.deleteBootcamp =(req , res , next)=>{
+    res.status(200).json({ success : true , msg : ` delete  bootcamps ${req.params.id}`});
+
+}
