fb2debe96f3436af3e937107ed19cde3189374a0
aliahmad1991 <aliahmadheydarzadeh@gmail.com>
2020-01-16 15:36:11 +0100

    login

---
+++ Dataserver/16-password-login-app/config/auth.js
@@ -0,0 +1,16 @@
+module.exports=function ensureAuthenticated(req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+
+    }
+    req.flash('error_msg','please log in to view the pages');
+    res.redirect('/users/login');
+
+}
+
+module.exports=function forwardAuthenticated(req,res,next){
+    if(!req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/dashboard');
+}
